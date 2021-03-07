4f3d1fa946b4a6b61a1902c13f5cfd6a2fc037c4
Ryan Potthoff <ryanpotthoff@Ryans-MBP.fios-router.home>
2021-03-07 16:48:28 -0500

    Updating to current drywal build. Added previously built drywal files.

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express')
 const app = express()
 const expressLayouts =  require('express-ejs-layouts')
-
-const indexRouter = require('./routes/index')
-
-
-
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -31,6 +25,7 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 
+const indexRouter = require('./routes/index')
 
 app.use('/', indexRouter)
 
